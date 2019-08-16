b8cc765fd0770e945203cccf91863eeeab850fc8
kenkoooo <kenkou.n@gmail.com>
2019-08-16 09:33:25 +0900

    Added title

--- src/Main.elm
+++ src/Main.elm
@@ -2,42 +2,76 @@ module Main exposing (main)
 
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
+import Browser exposing (..)
 import Html exposing (Html, a, div, h2, h5, p, text)
 import Html.Attributes exposing (href)
 
 
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd msg )
+init _ =
+    ( {}, Cmd.none )
+
+
+update : msg -> Model -> ( Model, Cmd msg )
+update _ _ =
+    ( {}, Cmd.none )
+
+
 main =
-    Grid.container []
-        [ CDN.stylesheet
-        , Grid.row []
-            [ Grid.col []
-                [ h2 [] [ text "Kenkou Nakamura" ]
-                , p [] [ text "I am a software engineer at Indeed." ]
+    Browser.document
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+subscriptions : Model -> Sub msg
+subscriptions model =
+    Sub.none
+
+
+view : model -> Document msg
+view model =
+    { title = "kenkoooo.com"
+    , body =
+        [ Grid.container []
+            [ CDN.stylesheet
+            , Grid.row []
+                [ Grid.col []
+                    [ h2 [] [ text "Kenkou Nakamura" ]
+                    , p [] [ text "I am a software engineer at Indeed." ]
+                    ]
                 ]
-            ]
-        , Grid.row []
-            [ Grid.col []
-                [ h2 [] [ text "Software" ]
-                , softwareBlock "AtCoder Problems" "https://kenkoooo.com/atcoder/"
-                , softwareBlock "Competitive Programming Snippets in Rust" "https://github.com/kenkoooo/competitive-programming-rs"
+            , Grid.row []
+                [ Grid.col []
+                    [ h2 [] [ text "Software" ]
+                    , softwareBlock "AtCoder Problems" "https://kenkoooo.com/atcoder/"
+                    , softwareBlock "Competitive Programming Snippets in Rust" "https://github.com/kenkoooo/competitive-programming-rs"
+                    ]
                 ]
-            ]
-        , Grid.row []
-            [ Grid.col []
-                [ h2 [] [ text "Experience" ]
-                , experienceBlock "Software Engineer at Indeed" "Mar 2019 -"
-                , experienceBlock "Software Engineer at SoundHound Inc." "Dec 2017 - Feb 2019"
-                , experienceBlock "Software Engineer at Recruit Communications Co., Ltd." "Apr 2016 - Nov 2017"
-                , experienceBlock "Software Engineer at National Institute of Informatics" "Oct 2015 - Mar 2016"
+            , Grid.row []
+                [ Grid.col []
+                    [ h2 [] [ text "Experience" ]
+                    , experienceBlock "Software Engineer at Indeed" "Mar 2019 -"
+                    , experienceBlock "Software Engineer at SoundHound Inc." "Dec 2017 - Feb 2019"
+                    , experienceBlock "Software Engineer at Recruit Communications Co., Ltd." "Apr 2016 - Nov 2017"
+                    , experienceBlock "Software Engineer at National Institute of Informatics" "Oct 2015 - Mar 2016"
+                    ]
                 ]
-            ]
-        , Grid.row []
-            [ Grid.col []
-                [ h2 [] [ text "Education" ]
-                , experienceBlock "Bachelar of Science, University of Tokyo" "Apr 2010 - Mar 2014"
+            , Grid.row []
+                [ Grid.col []
+                    [ h2 [] [ text "Education" ]
+                    , experienceBlock "Bachelar of Science, University of Tokyo" "Apr 2010 - Mar 2014"
+                    ]
                 ]
             ]
         ]
+    }
 
 
 softwareBlock : String -> String -> Html msg
